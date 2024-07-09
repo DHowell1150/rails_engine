902eec62b9b31a5745a489039444b5141f549a2c
lance butler <lancej2004@gmail.com>
2024-07-09 11:40:52 -0600

    Refactor: US/1/2 Serializers

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,40 +2,45 @@ require 'rails_helper'
 
 describe "Merchants API" do
     it "sends a list of merchants" do
-        create_list(:merchant, 3)
+      create_list(:merchant, 3)
 
-        get '/api/v1/merchants'
+      get '/api/v1/merchants'
 
-        expect(response).to be_successful
+      expect(response).to be_successful
 
-        merchants = JSON.parse(response.body, symbolize_names: true)
+      merchants = JSON.parse(response.body, symbolize_names: true)[:data]
 
-        expect(merchants.count).to eq(3)
+      expect(merchants.count).to eq(3)
 
-        merchants.each do |merchant|
-            expect(merchant).to have_key(:name)
-            expect(merchant[:name]).to be_a String
+      merchants.each do |merchant|
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a String
 
-            expect(merchant).to have_key(:id)
-            expect(merchant[:id]).to be_an Integer
-        end
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_an String
+
+        expect(merchant).to have_key(:type)
+        expect(merchant[:type]).to eq("merchant")
+
+        expect(merchant).to have_key(:attributes)
+        expect(merchant[:attributes]).to be_a Hash
+      end
     end
 
     it "can get one merchant by its id" do
-        id = create(:merchant).id
+      id = create(:merchant).id
 
-        get "/api/v1/merchants/#{id}"
+      get "/api/v1/merchants/#{id}"
 
-        merchant = JSON.parse(response.body, symbolize_names: true)
+      merchant = JSON.parse(response.body, symbolize_names: true)[:data]
 
-        expect(response).to be_successful
+      expect(response).to be_successful
 
-        expect(merchant).to have_key(:id)
-        expect(merchant[:id]).to eq(id)
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a(String)
 
-        expect(merchant).to have_key(:name)
-        expect(merchant[:name]).to be_a(String)
-
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
 
     it "can get items for a specific merchant" do
