41d5c2adde5278d8727ea21df0284489c4f60939
Garrett Bowman <garrettbowman248@gmail.com>
2024-07-12 08:02:12 -0500

    Lost logic yesterday

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/items/find_all', to: 'items#find_all'
       resources :items, only: [:index, :show, :create, :update, :destroy] do
-        get '/merchant', to: 'items#merchant'
+        get '/merchant', to: 'items_merchant#index', as: :merchant
       end
 
       get '/merchants/find', to: 'merchants#find'
